d2e251982c83e5e5cf476d4feb0a53990107f9fd
Adnan Hossain <adnanhossain274@gmail.com>
2021-05-07 07:40:09 +0600

    minor change

--- src/search/Main.java
+++ src/search/Main.java
@@ -12,7 +12,7 @@ public class Main {
             // the bottom line is used when the file location and name is given as a second argument through command
             // line. If the input file needs to be mentioned directly then uncomment the second Scanner instance.
             Scanner sc = new Scanner(new File(args[1]));
-            //Scanner sc = new Scanner(new File("File_Path\\inputFile.txt"));
+            // Scanner sc = new Scanner(new File("File_Path\\inputFile.txt"));
             ArrayList<String> list = new ArrayList<>();
             while (sc.hasNext()) {
                 list.add(sc.nextLine());
